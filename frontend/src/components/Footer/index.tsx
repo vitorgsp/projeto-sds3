7a59d1c4c2be1a3d4aceeb1823605273979c2fb8
Vitor Gabriel <vitorsp123@gmail.com>
2021-05-08 00:06:59 -0300

    Projeto criado e completo

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+const Footer = () => { //Forma diferente de declarar a função; Como se fosse um objeto;
+    return (
+
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/vitorgabrielpereira/" target="_blank" rel="noreferrer">Vitor Gabriel</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
